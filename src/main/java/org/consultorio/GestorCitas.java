45b359a55e5c6ee8407b9c0b915737c09c8ad609
Moises311993 <moisesfalling1993@gmail.com>
2025-02-09 21:23:36 -0600

    Crear integración de lectura/escritura de JSON para listado de citas. Incluir dependencia para escritura de formato de fechas.
    Verificado de excepciones de ObjectMapper

--- src/main/java/org/consultorio/GestorCitas.java
+++ src/main/java/org/consultorio/GestorCitas.java
@@ -1,14 +1,25 @@
 package org.consultorio;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class GestorCitas {
+public class GestorCitas implements Serializador{
     private List<Cita> citas = new ArrayList<>();
+    public static final String DB_DIR = "src/main/resources/db";
+    public static final String DB_CITAS = DB_DIR + "/citas.json";
+    private final static ObjectMapper objectMapper = new ObjectMapper();
 
-
+    public GestorCitas(){
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     public void registrarCita(Cita cita) {
         citas.add(cita);
@@ -33,5 +44,29 @@ public class GestorCitas {
         return buscarCita(id).isPresent();
     }
 
+    @Override
+    public <T> void guardarEnArchivo(String archivo, T objeto) {
+        File dbDir = new File(archivo).getParentFile();
+        if (!dbDir.exists()) {
+            dbDir.mkdirs();  // Crea la carpeta 'db' si no existe
+        }
+
+        try {
+
+            objectMapper.writeValue(new File(archivo), objeto);
+        } catch (IOException e) {
+            System.out.println("Error al guardar el archivo: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public <T> void cargarDesdeArchivo(String archivo, Class<T> tipo){
+        try {
+            citas = objectMapper.readValue(new File(archivo), new TypeReference<List<Cita>>() {
+            });
+        } catch (IOException e) {
+
+        }
+    }
 
 }
