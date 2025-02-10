4a8c22d47853925a7f338abf13f761835c78c3bc
Moises311993 <moisesfalling1993@gmail.com>
2025-02-09 21:17:23 -0600

    Agregar utilidades para la busqueda y listado de citas medicas

--- src/main/java/org/consultorio/GestorCitas.java
+++ src/main/java/org/consultorio/GestorCitas.java
@@ -3,6 +3,7 @@ package org.consultorio;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class GestorCitas {
     private List<Cita> citas = new ArrayList<>();
@@ -16,5 +17,21 @@ public class GestorCitas {
     public boolean eliminarCita(String id) {
         return citas.removeIf(c -> c.getId().equals(id));
     }
+
+    public List<Cita> listarCitas() {
+        return citas;
+    }
+
+    public Optional<Cita> buscarCita(String id){
+
+        return listarCitas().stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst();
+    }
+
+    public boolean existeCita(String id){
+        return buscarCita(id).isPresent();
+    }
+
 
 }
