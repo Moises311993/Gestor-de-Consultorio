9ed589d96fdd3f8dc17fde2f8718974d870a8446
Moises311993 <moisesfalling1993@gmail.com>
2025-02-09 21:27:41 -0600

    Crear integración de lectura/escritura de JSON para listado de usuarios.
    Verificado de excepciones de ObjectMapper

--- src/main/java/org/consultorio/GestorUsuarios.java
+++ src/main/java/org/consultorio/GestorUsuarios.java
@@ -1,11 +1,19 @@
 package org.consultorio;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-public class GestorUsuarios {
+public class GestorUsuarios implements Serializador{
+    private static final String DB_DIR = "src/main/resources/db";
+    public static final String DB_USUARIOS = DB_DIR + "/usuarios.json";
 
+    private final static ObjectMapper objectMapper = new ObjectMapper();
 
 
     private List<Usuario> usuarios;
@@ -38,5 +46,38 @@ public class GestorUsuarios {
     public List<Usuario> getUsuarios() {
         return usuarios;
     }
+
+    @Override
+    public <T> void guardarEnArchivo(String archivo, T objeto) {
+        File file = new File(archivo);
+        File dbDir = file.getParentFile();
+        dbDir.mkdirs();
+
+
+        try {
+            file.createNewFile();
+        }catch (IOException e){
+            System.out.println("Error al crear la base de datos de usuarios");
+        }
+
+        try {
+            objectMapper.writeValue(new File(archivo), objeto);
+        } catch (IOException e) {
+            System.out.println("Error al guardar el archivo: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public <T>  void cargarDesdeArchivo(String archivo, Class<T> tipo) {
+
+        try {
+
+                this.usuarios = objectMapper.readValue(new File(archivo), new TypeReference<List<Usuario>>() {
+                });
+
+        } catch (IOException e) {
+        }
+    }
+
 
 }
